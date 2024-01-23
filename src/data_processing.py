f01eff788456da7b26a371c8a62b1d4f26306996
Theodore G <95758907+theodoregi@users.noreply.github.com>
2024-01-23 18:13:06 +0100

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,11 +2,15 @@ import os
 import csv
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-R6  = "R6_2024_01_04_05_47_05_patrouille_TAC_dechetterie_clean"
-R11 = "R11_2024_01_04_07_58_48_parc_check_clean"
+import numpy as np
+
+dtype = [('s', 'int64'), ('ns', 'int64'), ('x', 'float64'), ('y', 'float64'), ('z', 'float64')]
+
+
 
 def read_data_files(data_dir):
     all_positions = []
+
     base_dir = "data/"
     for dir in os.listdir(base_dir):
             if dir.startswith(data_dir):
@@ -19,21 +23,22 @@ def read_data_files(data_dir):
                             positions = []
                             for row in reader:
                                 if row.__contains__("pose.pose.position.x"):
-                                    positions.append([float(row["pose.pose.position.x"]), float(row["pose.pose.position.y"]), float(row["pose.pose.position.z"])])
-                            all_positions.append(positions)
-    return all_positions
+                                    positions = np.array([float(row["header.stamp.secs"]),float(row["header.stamp.nsecs"]),float(row["pose.pose.position.x"]), float(row["pose.pose.position.y"]), float(row["pose.pose.position.z"])], dtype=dtype)
+                                all_positions.append(positions)
+    return cerbere, imu7, odom_hdl
 
-# Plot positions in 3D
-positions = read_data_files("R6")
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-for pos in positions:
-    x = [p[0] for p in pos]
-    y = [p[1] for p in pos]
-    z = [p[2] for p in pos]
-    ax.plot(x, y, z)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
+print(read_data_files("R6"))
+
+# positions = read_data_files("R6")
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# for pos in positions:
+#     x = [p[0] for p in pos]
+#     y = [p[1] for p in pos]
+#     z = [p[2] for p in pos]
+#     ax.plot(x, y, z)
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+# plt.show()
 
